5c43fd86e4b9da7d902d94a2334fec955e33cb8c
Sam Davenport <samueldavenport@att.net>
2021-12-08 12:17:24 -0600

    database created and populated with data

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+Drop TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS job_title;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE job_title (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_job_title FOREIGN KEY (job_title_id) REFERENCES job_title(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
